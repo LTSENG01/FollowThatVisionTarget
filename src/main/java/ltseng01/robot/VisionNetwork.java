04c4d38d8c72630101fe3e76f1bba546001ef3fe
Larry Tseng <lt222888@gmail.com>
2019-01-16 22:12:47 -0500

    Add NT listeners and clean up VisionObjectDetails

--- src/main/java/ltseng01/robot/VisionNetwork.java
+++ src/main/java/ltseng01/robot/VisionNetwork.java
@@ -1,59 +1,92 @@
 package ltseng01.robot;
 
+import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 import java.util.HashMap;
 
+/**
+ *
+ */
 public class VisionNetwork {
 
     private static NetworkTable visionTable;
+    private static NetworkTable visionDataTable;
 
-    private static HashMap<String, VisionEntity> visionData;
+    private static volatile HashMap<VisionType, VisionObjectDetails> visionData;
 
     enum VisionType {
-        CARGO, PANEL, VISION_TARGET, FLOOR_TAPE;
+        CARGO, PANEL, VISION_TARGET, FLOOR_TAPE
     }
 
-    class VisionEntity {
+    static class VisionObjectDetails {
 
         private int count;
-        private VisionType object_type;
-        private int number;
-        private double elevation_angle;
+        private double elevationAngle;
         private double azimuth;
         private double distance;
 
-        public VisionEntity(int count, VisionType object_type, int number, double elevation_angle, double azimuth, double distance) {
+        VisionObjectDetails(int count, double elevationAngle, double azimuth, double distance) {
             this.count = count;
-            this.object_type = object_type;
-            this.number = number;
-            this.elevation_angle = elevation_angle;
+            this.elevationAngle = elevationAngle;
             this.azimuth = azimuth;
             this.distance = distance;
         }
+
+        public int getCount() {
+            return count;
+        }
+
+        public double getElevationAngle() {
+            return elevationAngle;
+        }
+
+        public double getAzimuth() {
+            return azimuth;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+
 
     }
 
     static {
 
         visionTable = NetworkTableInstance.getDefault().getTable("Vision");
-        visionData = new HashMap<>();
+        visionDataTable = visionTable.getSubTable("data");
+
+        visionData = new HashMap<>(4);
+
+
+        // Register NetworkTables Listeners
+
+        for (VisionType type : VisionType.values()) {
+
+            visionTable.addEntryListener(type.toString(), (table, key, entry, value, flags) -> {
+                double[] objectValues = value.getDoubleArray();
+                visionData.put(type,
+                        new VisionObjectDetails(
+                                (int) objectValues[0],
+                                objectValues[1],
+                                objectValues[2],
+                                objectValues[3]));
+            }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+        }
 
     }
 
-    public static void refreshVisionData() {
-
-        // A. read every loop
-        // B. use NT listeners
-
-        // if count isn't updated
-
+    public static VisionObjectDetails getVisionObjectDetails(VisionType visionType) {
+        return visionData.get(visionType);
     }
+
 
     // Send start to vision
     // Send stop to vision
 
-    // check if data is stale --> delete
+    // check if data is stale --> delete?
 
 }
